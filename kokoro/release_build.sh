dab5953875d967284b266bdf9157817ab05eff34
Chanseok Oh <chanseok@google.com>
2020-06-04 17:31:40 -0400

    Fix working directory

--- kokoro/release_build.sh
+++ kokoro/release_build.sh
@@ -5,8 +5,9 @@ set -o errexit
 # Display commands to stderr.                                                                                                                                                                                     
 set -o xtrace                                                                                                                                                                                                     
                                                                                                                                                                                                                   
+cd github/jib-extensions                                                                                                                                                                                          
+
 git describe --tags  # non-zero exit code (no tag found) will fail the script                                                                                                                                     
 readonly EXTENSION_NAME=$( git describe --tags | sed -e 's/^v[0-9.]\+-//')                                                                                                                                        
                                                                                                                                                                                                                   
-cd github/jib-extensions                                                                                                                                                                                          
 ./gradlew ":${EXTENSION_NAME}:prepareRelease"                                                                                                                                                                     
