c9afc71e345f933389517457ed6aad597eb602dc
Chanseok Oh <chanseok@google.com>
2020-06-04 16:14:42 -0400

    Add Kokoro release build script

---
+++ kokoro/release_build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh                                                                                                                                                                                                         
+                                                                                                                                                                                                                  
+# Fail on any error.                                                                                                                                                                                              
+set -o errexit                                                                                                                                                                                                    
+# Display commands to stderr.                                                                                                                                                                                     
+set -o xtrace                                                                                                                                                                                                     
+                                                                                                                                                                                                                  
+git describe --tags  # non-zero exit code (no tag found) will fail the script                                                                                                                                     
+readonly EXTENSION_NAME=$( git describe --tags | sed -e 's/^v[0-9.]\+-//')                                                                                                                                        
+                                                                                                                                                                                                                  
+cd github/jib-extensions                                                                                                                                                                                          
+./gradlew ":${EXTENSION_NAME}:prepareRelease"                                                                                                                                                                     
